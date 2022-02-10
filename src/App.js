257358f16997f383eb145d8a5237b6954e4aeb7d
Ade <shennaike2386@hotmail.com>
2022-02-10 00:07:52 -0500

    built the usedarkmode hook and it renders and works properly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
-import Charts from "./components/Charts";
+import Charts from './components/Charts';
 import Navbar from "./components/Navbar";
-
+import useDarkMode from './components/hooks/useDarkMode';
 import "./style.css";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [coinData, darkMode, setDarkMode] = useDarkMode('');
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
